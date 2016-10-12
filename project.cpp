fcc0548e61625cf34469e89811c7bfcf5f14a2c1
LucaMazza99 <thelucraft99@gmail.com>
2016-10-12 15:46:55 +0200

    Update project.cpp
    Sistemato ItemPriority 
    
    Problema con movimento verso oggetto molto vicino -> La sfera rimane ferma

--- project.cpp
+++ project.cpp
@@ -10,6 +10,29 @@ float facing[3];
 float vel[3];
 float speed;
 float zona[4];
+bool LARGE, MEDIUM, SMALL;
+int ID;
+bool spsAllDrop;
+
+void allontana(){
+   for(int i=0; i<3; i++)
+     if(vai[i]>0)
+       vai[i] = vai[i]-0.03;
+     else{
+       vai[i] = abs(vai[i])-0.03;
+       vai[i] = -vai[i];
+     }
+}
+
+void avvicina(){
+    for(int i=0; i<3; i++)
+     if(vai[i]>0)
+       vai[i] = vai[i]+0.03;
+     else{
+       vai[i] = abs(vai[i])+0.03;
+       vai[i] = -vai[i];
+     }
+}
 
 float dist(float a[],float b[])/*Definitivo*/{
 	return sqrt(mathSquare(a[0] - b[0]) + mathSquare(a[1] - b[1]) + mathSquare(a[2] - b[2]));
@@ -27,14 +50,18 @@ void setV(float v[],float x,float y,float z)/*Definitivo*/{
 }
 
 bool oOB(float ptc[])/*Definitivo*/{
-	return fabsf(ptc[0]) > 0.75 || fabsf(ptc[1]) > 0.75 || fabsf(ptc[2]) > 0.75;
+	if((fabsf(ptc[0]) > 0.75) || (fabsf(ptc[1]) > 0.75) || (fabsf(ptc[2]) > 0.75))
+      return true;
+    return false;
 }
 
-void ruota()/*Definitivo*/{
+void ruota(){
 	float v[3];
-	mathVecSubtract(v,punta,pos,3);
-	mathVecNormalize(v,3);
-	api.setAttitudeTarget(v);
+	if(spsAllDrop && dist(pos,vai) < 0.5){//per risparmiare fuel
+	  mathVecSubtract(v,punta,pos,3);
+	  mathVecNormalize(v,3);
+	  api.setAttitudeTarget(v);
+	}
 }
 
 void muovi(){
@@ -46,15 +73,26 @@ void muovi(){
 		float vec[3],d = dist(vai,pos);
 		DEBUG(("%f",d));
 		mathVecSubtract(vec,vai,pos,3);
-		if(d > 0.50)
-			api.setVelocityTarget(vec);
-		api.setPositionTarget(vai);
-		if(d < 0.1)
-			api.setPositionTarget(pos); //sostituisce frena())
+		switch(spsAllDrop)
+		{
+		    case true: 
+		      if(d > 0.267)
+		        api.setPositionTarget(vai);
+		      else 
+		        api.setPositionTarget(pos);
+		    break;
+		    case false:	
+		      if(d > 0.50)
+			    api.setVelocityTarget(vec);
+		      api.setPositionTarget(vai);
+		      if(d < 0.13)
+			    api.setPositionTarget(pos); //sostituisce frena())
+			break;
+		}
 	}
 }
 
-int checkDock(int id)/*boh*/{
+int checkDock(int id)/*verifico condizioni per docking*/{
 	float targ[3];
 	game.getItemLoc(targ,id); //Posizione dell'oggetto
 	float targD = dist(targ,pos); //Distanza dall'oggetto 
@@ -76,10 +114,32 @@ int checkDock(int id)/*boh*/{
 	}
 	return 0;
 }
-void dock(int id){
-	if(checkDock(id))/*docko*/{
-		game.dockItem();
-	}
+
+void dock(){
+    switch (checkDock(ID)){
+        case 0:
+          game.dockItem();
+          if (game.hasItemBeenPickedUp(ID) || game.hasItemBeenPickedUp(ID+1))
+          switch (ID){
+              case 0: case 1: LARGE = false; break;
+              case 2: case 3: MEDIUM = false; break;
+              case 4: case 5: SMALL = false; break;
+          }
+        break;
+        case 1:
+          allontana();
+        break;
+        case 2:
+          if(dist(pos,punta) < 0.45)
+            avvicina();
+          else
+            api.setPositionTarget(vai);
+        break;
+        case 3:
+          api.setPositionTarget(pos);
+        break;
+        default: DEBUG (("ERRORE")); break;
+    }
 }
 
 void init(){
@@ -87,6 +147,11 @@ void init(){
 	api.getMyZRState(stato);
 	BoR = stato[1] > 0 ? 'R' : 'B'; //capisco quale sfera siamo
 	fase = sottofase = 0;
+	spsAllDrop = false;
+	LARGE = true;    //Partono con true poichè ancora presenti nel campo, passano a false se non presenti
+	MEDIUM = true;
+	SMALL = true;
+	ID = 0;
 }
 
 void inizio(){
@@ -106,24 +171,52 @@ void fine(){
 	muovi(); //spostamento verso vai[]
 }
 
+void itemPriority(){
+  /*float large[3]={0.375, 0.727, 0.2}; //massa, accelerazione, punteggio
+    float medium[3]={0.25, 0.8, 0.15};
+    float small[3]={0.125, 0.889, 0.1};*/
+    float d[3]={0.0};
+    float d1[3]={0.0};
+    
+    if (!LARGE)
+        ID = 2;
+    if (!MEDIUM)
+        ID = 4;
+    if (!SMALL)
+    {
+        ID = 2;
+        MEDIUM = true;
+    }
+    
+    game.getItemLoc(d,ID);
+    game.getItemLoc(d1,(ID+1));
+    if((dist(pos, d)+dist(d, zona)) > (dist(pos, d1)+dist(d1, zona)) && !game.hasItemBeenPickedUp(ID+1)){    //restituisce l'id dell'item più conveniente da andare a prendere
+       DEBUG(("ID: %d", ID+1));
+       ID++;
+    }else
+       DEBUG(("ID: %d", ID));
+}
+
 void loop(){
     inizio();
 	switch(fase){
         case 0://drop SPSs, i cosi per sapere dove si trova la zhohona
-            if (sottofase==0 && dist(vai,pos)<0.1){
+            if (sottofase==0){
                 game.dropSPS();
                 sottofase++;
-                setV(vai, -0.4, -0.5, 0);
+                setV(vai, -0.3, -0.5, 0);
                 //game.getItemLoc(vai,1);
             }
             else if (sottofase==1 && dist(vai,pos)<0.1){
                 game.dropSPS();
                 sottofase++;
-                setV(vai, -0.2, 0.3, 0);
+                setV(vai, -0.3, 0.3, 0);
                 //game.getItemLoc(vai,5);
             }
             else if(sottofase==2 && dist(vai,pos)<0.1){
                 game.dropSPS();
+                spsAllDrop = true;
+                game.getZone(zona);
                 sottofase=0;
                 fase++;
             }
@@ -136,5 +229,19 @@ void loop(){
         default:
         DEBUG(("ERROR"));
 	}
+	if(spsAllDrop == true && game.hasItem(ID) == 0){
+          itemPriority();
+          DEBUG (("ID: %d", ID));
+          game.getItemLoc(vai, ID);
+          game.getItemLoc(punta, ID);
+          dock();
+    }
+    else if(spsAllDrop){
+        game.getZone(zona);
+        setV(vai, zona);
+        setV(punta, vai);
+        if(dist(pos,vai) < 0.185)
+          game.dropItem();
+    }
 	fine();
 }
